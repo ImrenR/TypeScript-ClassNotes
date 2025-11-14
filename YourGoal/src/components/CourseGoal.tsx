35d4444f8d1b257a115908228e9ae25691ed98da
ImrenR <imrenrahbay@gmail.com>
2025-11-14 20:05:01 +0300

    update

--- YourGoal/src/components/CourseGoal.tsx
+++ YourGoal/src/components/CourseGoal.tsx
@@ -1,10 +1,14 @@
 
 const CourseGoal = () => {
   return (
+    <article>
     <div>
 
-      
+      <h2>Title</h2>
+      <p>description</p>
     </div>
+    <button>Delete</button>
+    </article>
   )
 }
 
