582dc873c939752ec817f30ddbb8197f94957a9a
ImrenR <imrenrahbay@gmail.com>
2025-11-15 22:52:33 +0300

    up

--- YourGoal/src/components/NewGoal.tsx
+++ YourGoal/src/components/NewGoal.tsx
@@ -2,13 +2,14 @@ import { useRef, type FormEvent } from "react";
 
 const NewGoal = () => {
 
- const goal = useRef(null);
- const summary = useRef(null);
+ const goal = useRef<HTMLInputElement>(null);
+ const summary = useRef<HTMLInputElement>(null);
 
 
   const handleSubmit = (e : FormEvent<HTMLFormElement>)=> {
     e.preventDefault();
-  const enteredGoal=goal.current!.value
+  const enteredGoal=goal.current!.value;
+  const enteredSummary=summary.current!.value;
   }
   return (
    <form onSubmit={handleSubmit}>
