a110a63648f137276cf330320de4d3bab2def000
ImrenR <imrenrahbay@gmail.com>
2025-11-15 19:09:52 +0300

    update

--- YourGoal/src/components/CourseGoal.tsx
+++ YourGoal/src/components/CourseGoal.tsx
@@ -1,27 +1,24 @@
-import type { PropsWithChildren} from "react";
+import { type PropsWithChildren } from "react";
 
 // interface CourGoalProps {
 //   title: string
 //   children: ReactNode
 // }
 
-//! alternative way 2. : 
+//! alternative way 2. :
 
-type CourseGoalProps = PropsWithChildren<{title: string}>
+type CourseGoalProps = PropsWithChildren<{ title: string }>;
 
 //! alternative way :
 //const CourseGoal : FC <CourseGoalProps> = ({children, title}) => { dont forget to import FC from react
 
-const CourseGoal = ({children, title}: CourseGoalProps) => {
+const CourseGoal = ({ children, title }: CourseGoalProps) => {
   return (
     <div className="flex flex-col items-center  bg-gray-200">
       {/* Card container */}
       <div className="bg-gray-100 rounded-lg py-5 my-auto w-full max-w-md flex flex-col gap-4">
         {/* Label */}
-        <label
-          htmlFor="title"
-          className="text-sm font-medium text-gray-900"
-        >
+        <label htmlFor="title" className="text-sm font-medium text-gray-900">
           {title}
         </label>
 
@@ -37,6 +34,7 @@ const CourseGoal = ({children, title}: CourseGoalProps) => {
           className="py-2 px-3 rounded-md border border-gray-300 bg-gray-50 text-gray-900 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
         />
       </div>
+
       <div>
         <button>DELETE</button>
       </div>
