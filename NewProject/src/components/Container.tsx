605af486f0043d4e1d6e3a2974f7734fae731791
ImrenR <imrenrahbay@gmail.com>
2025-11-16 17:36:23 +0300

    up

--- NewProject/src/components/Container.tsx
+++ NewProject/src/components/Container.tsx
@@ -1,8 +1,17 @@
+import type { ComponentPropsWithoutRef, ElementType, ReactNode } from "react"
 
-const Container = () => {
-  return (
-    <div>Container</div>
-  )
+type ContainerProps <T extends ElementType>={
+as: T;
+children: ReactNode;
+} & ComponentPropsWithoutRef<T>;
+
+function Container<C extends ElementType>({as, children, ...props}: ContainerProps<C>)  {
+  
+  const Component = as || "div";
+
+  return <Component {...props}>
+{children}
+  </Component>
 }
 
-export default Container
+export default Container;
