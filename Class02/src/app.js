6cccaba55a0683aa3292d4ecfe8d495e236158ac
ImrenR <imrenrahbay@gmail.com>
2025-11-17 12:36:50 +0300

    example

--- Class02/src/app.js
+++ Class02/src/app.js
@@ -1,3 +1,8 @@
 "use strict";
 //Fonksiyonlar 
 //! Fonksiyonu void olarak tanimlama
+function toplam(a, b) {
+    console.log(a + b);
+    return;
+}
+toplam(34, 56);
--- Class02/src/app.ts
+++ Class02/src/app.ts
@@ -2,3 +2,9 @@
 
 //! Fonksiyonu void olarak tanimlama
 
+function toplam(a:number, b:number):void{
+  console.log(a+b)
+  return
+}
+
+toplam(34,56)
