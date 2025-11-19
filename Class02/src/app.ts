b70b4ca84b935d375af550a970e8c44abe403b36
ImrenR <imrenrahbay@gmail.com>
2025-11-19 23:17:17 +0300

    update

--- Class02/src/app.ts
+++ Class02/src/app.ts
@@ -3,16 +3,16 @@
 //! 1. Fonksiyonu void olarak tanimlama
 
 function toplam(a:number, b:number):void{
-  console.log(a+b)
+  console.log(a+b);
   return
 }
 
-toplam(34,56)
+toplam(34,56);
 
 //! 2. Istege bagli parametreler
 
 // Bazi parametlerede istege bagli olmasi icin ? kullanilir.
-// optional manasindadir.
+// optional manasindadir..
 
 function selamla(msj:string, name:string, lastname?:string):void{
   console.log(`${name}`)
